29b32690b0938b696ede461607c7222f33525b89
emckean <emckean@Erins-MacBook-Pro-2.local>
2017-03-23 09:44:38 -0700

    add explanatory note to temp-config

--- pigify/temp-config.js
+++ pigify/temp-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //Your Twitter credentials go in this file
+  //Your Twitter credentials go in this file; don't include the '@' in your bot_name
   consumer_key:         '',
   consumer_secret:      '',
   access_token:         '',
