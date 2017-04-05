184807223f898615cb8838d3be2ff122f49c8649
emckean <emckean@erins-mbp-2.usca.ibm.com>
2017-04-05 15:43:21 -0700

    remove config file

--- pigify/temp-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  //Your Twitter credentials go in this file; don't include the '@' in your bot_name
-  consumer_key:         '',
-  consumer_secret:      '',
-  access_token:         '',
-  access_token_secret:  '',
-  bot_name: ''
-};
